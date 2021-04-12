d948700d73748f3e305fd0a209503de09d1df0f2
jinyu <jinxu_titan@126.com>
2021-04-12 17:46:53 +0800

    get vedio url list

--- ytEduSpider.py
+++ ytEduSpider.py
@@ -48,7 +48,8 @@ class ytEduCrawler:
         wait.until(EC.presence_of_element_located((By.XPATH, '/html/body')))
         input_name = self.chrome.find_element_by_id("txtName")
         input_passwd = self.chrome.find_element_by_id("pswPassword")
-        input_name.send_keys("135")
+        # input_name.send_keys("13770273837")
+        input_name.send_keys("13584783806")
         input_passwd.send_keys("123456")
         self.chrome.find_element_by_id("login").click()
         time.sleep(5)
@@ -67,9 +68,12 @@ class ytEduCrawler:
         # 左侧课程导航，点击
         EC.presence_of_element_located((By.CLASS_NAME, "content_left"))
         course_list = self.chrome.find_elements_by_xpath("//ul[@class='content_left']//li")
+        course_url_list = []
+        course_chapter_video_list = []
         for course_index in range(1, len(course_list)+1):
             time.sleep(2)
             course = self.chrome.find_element_by_xpath("//ul[@class='content_left']/li["+str(course_index)+"]")
+            course_name = course.text
             course.click()
             EC.presence_of_element_located((By.CLASS_NAME, "content_rig"))
             time.sleep(2)
@@ -81,12 +85,16 @@ class ytEduCrawler:
                 current_course.click()
                 time.sleep(2)
                 chapter = self.chrome.find_element_by_xpath("//ul[@class='content_rig']/li["+str(chapter_index)+"]")
+                chapter_name = chapter.text
                 chapter.click()
                 time.sleep(5)
-                video_list = self.chrome.find_elements_by_xpath("//li[@class='clearfix']")
+                # video_list = self.chrome.find_elements_by_xpath("//li[@class='clearfix']")
+                video_list = self.chrome.find_elements_by_xpath("//div[@class='listPart']//li[@class='clearfix']")
                 if len(video_list) >= 2:
                     for video_index in range(2, len(video_list)+1):
-                        video = self.chrome.find_element_by_xpath("//li[@class='clearfix']["+str(video_index)+"]")
+                        video = self.chrome.find_element_by_xpath("//div[@class='listPart']//li[@class='clearfix']["+str(video_index)+"]")
+                        video_name = video.text
+                        course_chapter_video_list.append(course_name+"_"+chapter_name+"_"+video_name)
                         video.click()
                         time.sleep(5)
                 self.chrome.back()
@@ -113,9 +121,15 @@ class ytEduCrawler:
 
         request_logs = self.proxy.har
         for entry in request_logs['log']['entries']:
-            _url = entry['request']['url']
-            if "m3u8" in _url:
-                print(_url)
+            m3u8_url = entry['request']['url']
+            if "m3u8" in m3u8_url:
+                print(m3u8_url)
+                course_url_list.append(m3u8_url)
+        with open("info.txt","w") as f:
+            for name in course_chapter_video_list:
+                f.write(name + '\n')
+                for _url in course_url_list:
+                    f.write(_url + '\n')
         originResp = self.chrome.page_source
         return originResp
 
