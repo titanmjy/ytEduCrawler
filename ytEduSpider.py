0cfe795d517ec27782002af30d6df8c4d78f68db
jinyu <jinxu_titan@126.com>
2021-05-06 16:28:20 +0800

    add get_knowledges() method

--- ytEduSpider.py
+++ ytEduSpider.py
@@ -15,21 +15,23 @@ class ytEduCrawler:
     domain = "ksbao.com"
     start_url = "https://wide.ksbao.com/login"
 
-    def __init__(self, domain=domain):
-        # 本地代理server
-        self.server = Server(r'D:\Programs\browsermob-proxy-2.1.4-bin\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat')
-        self.server.start()
-        self.proxy = self.server.create_proxy()
+    # 下载视频需要server参数，默认为None表示非下载视频的情况
+    def __init__(self, domain=domain, server=None ):
         chrome_options = Options()
-        chrome_options.add_argument('--proxy-server={0}'.format(self.proxy.proxy))
         chrome_options.add_argument('--incognito')
         chrome_options.add_argument('--ignore-certificate-errors')
         chrome_options.add_argument('--disable-gpu')
+        self.server = server
+        if server is not None:
+            server.start()
+            self.proxy = server.create_proxy()
+            chrome_options.add_argument('--proxy-server={0}'.format(self.proxy.proxy))
+            self.proxy.new_har(domain)
         self.chrome = webdriver.Chrome(chrome_options=chrome_options)
-        self.proxy.new_har(domain)
 
     def quit(self):
-        self.server.stop()
+        if self.server is not None:
+            self.server.stop()
         self.chrome.quit()
         time.sleep(3)
 
@@ -62,7 +64,7 @@ class ytEduCrawler:
         EC.presence_of_element_located((By.CLASS_NAME, "content_left"))
         menu_list = self.chrome.find_elements_by_xpath("//ul[@class='content_left']//li")
         current_index_count = 0
-        for menu_index in range(1, len(menu_list)+1):
+        for menu_index in range(1, len(menu_list) + 1):
             time.sleep(2)
             menu = self.chrome.find_element_by_xpath("//ul[@class='content_left']/li[" + str(menu_index) + "]")
             # 点击左侧课程目录
@@ -71,9 +73,10 @@ class ytEduCrawler:
             time.sleep(2)
             chapter_length = len(self.chrome.find_elements_by_xpath("//ul[@class='content_rig']//li"))
             chapter_index = 0
-            for i in range(1, chapter_length+1):
-                current_chapter_size_desc = self.chrome.find_element_by_xpath("//ul[@class='content_rig']/li["+str(i)+"]/span").text
-                current_chapter_size = int(re.sub("\D","",current_chapter_size_desc))
+            for i in range(1, chapter_length + 1):
+                current_chapter_size_desc = self.chrome.find_element_by_xpath(
+                    "//ul[@class='content_rig']/li[" + str(i) + "]/span").text
+                current_chapter_size = int(re.sub("\D", "", current_chapter_size_desc))
                 video_index = start_index - current_index_count
                 current_index_count += current_chapter_size
                 chapter_index += 1
@@ -98,7 +101,7 @@ class ytEduCrawler:
     def vedio_download(self, name_list, url_list):
         for name, _url in zip(name_list, url_list):
             print(name + ":" + _url)
-            os.system("start N_m3u8DL-CLI " + _url + " --saveName " + '"'+ name +'"')
+            os.system("start N_m3u8DL-CLI " + _url + " --saveName " + '"' + name + '"')
             time.sleep(7)
 
     # 控制浏览器依次点击视频，并记录视频名称，左开右闭，start_index: 视频编号，从1开始,end_index不包括
@@ -106,11 +109,11 @@ class ytEduCrawler:
         menu_chapter_video_list = []
         menu_start, chapter_start, video_start = self.seek_chapter_start(start_index, end_index)
         menu_list = self.chrome.find_elements_by_xpath("//ul[@class='content_left']//li")
-        for menu_index in range(menu_start, len(menu_list)+1):
+        for menu_index in range(menu_start, len(menu_list) + 1):
             time.sleep(2)
             menu = self.chrome.find_element_by_xpath("//ul[@class='content_left']/li[" + str(menu_index) + "]")
             menu_name = menu.text
-            # 点击左侧课程目录
+            # 点击左侧导航目录
             menu.click()
             EC.presence_of_element_located((By.CLASS_NAME, "content_rig"))
             time.sleep(2)
@@ -118,7 +121,8 @@ class ytEduCrawler:
             for chapter_index in range(chapter_start, len(chapter_list) + 1):
                 time.sleep(2)
                 # 点击左侧具体课程目录
-                current_menu = self.chrome.find_element_by_xpath("//ul[@class='content_left']/li[" + str(menu_index) + "]")
+                current_menu = self.chrome.find_element_by_xpath(
+                    "//ul[@class='content_left']/li[" + str(menu_index) + "]")
                 current_menu.click()
                 time.sleep(2)
                 # 点击右侧章节，直接播放当前章节第一个video
@@ -128,10 +132,10 @@ class ytEduCrawler:
                 time.sleep(5)
                 video_list = self.chrome.find_elements_by_xpath("//div[@class='listPart']//li[@class='clearfix']")
                 for video_index in range(video_start, len(video_list) + 1):
-                    video = self.chrome.find_element_by_xpath("//div[@class='listPart']//li[@class='clearfix'][" + str(video_index) + "]")
+                    video = self.chrome.find_element_by_xpath(
+                        "//div[@class='listPart']//li[@class='clearfix'][" + str(video_index) + "]")
                     video_name = video.text
                     menu_chapter_video_list.append(menu_name + "_" + chapter_name + "_" + video_name)
-                    # knowledgeText
                     if len(video_list) > 1:
                         video.click()
                         time.sleep(5)
@@ -142,45 +146,80 @@ class ytEduCrawler:
                     break
             if len(menu_chapter_video_list) >= (end_index - start_index):
                 break
-            #         # 视频列表
-            #         EC.presence_of_element_located((By.TAG_NAME, "video"))
-            #         time.sleep(5)
-            #         video_list = self.chrome.find_elements_by_class_name("summary")
-            #         for video in video_list:
-            #             video.click()
-            #             time.sleep(10)
-            #             self.chrome.back()
-            #             # result = self.proxy.har
-            #             # print(result['log']['entries'])
-            #             # for entry in result['log']['entries']:
-            #             #     _url = entry['request']['url']
-            #             #     print(_url)
-            #         # 考点详情
-            #         # "//pre"
-            #         # 考点精讲
-            #         # //div[@class='testBtn subBtnColor']
-            #         # 点击背题模式，选择目标div
-            #         # //div[@class='exambt']
-            #         self.chrome.back()
         return menu_chapter_video_list[start_index - end_index:end_index]
 
     # 左开右闭，start_index: 视频编号，从1开始,end_index不包括
     def get_videos(self, start_index, end_index):
         # 访问指定数量视频的页面
         name_list = self.visit_and_get_name_list(start_index, end_index)
-        # 获取视频地址
+        # 获取视频url地址
         course_url_list = self.get_m3u8_url_list(start_index, end_index)
         self.vedio_download(name_list, course_url_list)
+
+    def get_knowledges(self):
+        # 点击考点精讲
+        self.chrome.find_element_by_class_name("rout-kdjj").click()
+        time.sleep(2)
+        self.chrome.refresh()
+        time.sleep(2)
+        menu_chapter_video_list = []
+        # 左侧课程导航目录
+        EC.presence_of_element_located((By.CLASS_NAME, "content_left"))
+        menu_list = self.chrome.find_elements_by_xpath("//ul[@class='content_left']//li")
+        for menu_index in range(1, len(menu_list) + 1):
+            time.sleep(2)
+            menu = self.chrome.find_element_by_xpath("//ul[@class='content_left']/li[" + str(menu_index) + "]")
+            menu_name = menu.text
+            # 点击左侧导航目录
+            menu.click()
+            EC.presence_of_element_located((By.CLASS_NAME, "content_rig"))
+            time.sleep(2)
+            chapter_list = self.chrome.find_elements_by_xpath("//ul[@class='content_rig']//li")
+            for chapter_index in range(1, len(chapter_list) + 1):
+                time.sleep(2)
+                # 点击左侧具体课程目录
+                current_menu = self.chrome.find_element_by_xpath(
+                    "//ul[@class='content_left']/li[" + str(menu_index) + "]")
+                current_menu.click()
+                time.sleep(2)
+                # 点击右侧章节，直接播放当前章节第一个video
+                chapter = self.chrome.find_element_by_xpath("//ul[@class='content_rig']/li[" + str(chapter_index) + "]")
+                chapter_name = chapter.text
+                chapter.click()
+                time.sleep(5)
+                video_list = self.chrome.find_elements_by_xpath("//div[@class='listPart']//li[@class='clearfix']")
+                for video_index in range(1, len(video_list) + 1):
+                    video = self.chrome.find_element_by_xpath("//div[@class='listPart']//li[@class='clearfix'][" + str(video_index) + "]")
+                    if len(video_list) > 1:
+                        video.click()
+                        time.sleep(5)
+                    video_name = video.text
+                    menu_chapter_video_list.append(menu_name + "_" + chapter_name + "_" + video_name)
+                    self.chrome.find_element_by_xpath("//div[@class='tabDiv']/span[2]").click()
+                    time.sleep(1)
+                    knowledge_text_div_ele = None
+                    try:
+                        knowledge_text_div_ele = self.chrome.find_element_by_class_name("knowledgeText")
+                    except Exception as e:
+                        print(e)
+                    if knowledge_text_div_ele is not None:
+                        knowledge_text = knowledge_text_div_ele.text
+                        print(knowledge_text)
+                    self.chrome.find_element_by_xpath("//div[@class='tabDiv']/span[1]").click()
+                    time.sleep(1)
+                self.chrome.back()
 
     def start_request(self, url=start_url):
         try:
             # 首页登录
             self.login(url)
-            self.get_videos(start_index=216, end_index=225)
+            # self.get_videos(start_index=1, end_index=3)
+            self.get_knowledges()
         finally:
             self.quit()
 
 
 if __name__ == "__main__":
+    server = Server(r'D:\Programs\browsermob-proxy-2.1.4-bin\browsermob-proxy-2.1.4\bin\browsermob-proxy.bat')
     spider = ytEduCrawler()
     spider.start_request()
